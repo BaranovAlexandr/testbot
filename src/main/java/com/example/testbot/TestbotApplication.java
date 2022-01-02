2831d9e27e95388c3dcb94d97b5391e7dc7bcfb4
BaranovAlex <kooppex@gmail.com>
2022-01-02 20:41:50 +0300

    1

--- src/main/java/com/example/testbot/TestbotApplication.java
+++ src/main/java/com/example/testbot/TestbotApplication.java
@@ -2,11 +2,21 @@ package com.example.testbot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.telegram.telegrambots.meta.TelegramBotsApi;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+@Controller
 @SpringBootApplication
 public class TestbotApplication {
+
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		return "Hello World!";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestbotApplication.class, args);
